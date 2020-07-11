9fd30d3629909585c77d2ed31dff7f2f6fe05d8d
Susana Santamaria <susana.s7@hotmail.com>
2020-07-10 19:09:33 -0500

    style: details of product for users

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,26 +9,32 @@
             <div class="card-header justify-content-center">
                 <div class="row">
                     @foreach($products as $product)
-                        <div class="col-md-4 tect-md-center">
-                            <img src="{{ URL::to('/images/' . $product->img_route) }}" width="300" >
+                        <div class="col-md-4 tect-md-center" align="center">
+                            <br><br><img src="{{ URL::to('/images/' . $product->img_route) }}" style="width:250px ; heigth:250px">
                             <!--<img src="images/{{$product->img_route}}" width="75">-->
                             <h4>{{ $product->title }}</h4>
                             <h4>Precio COP {{ $product->price }} </h4>
                             <a class="btn btn-primary" data-toggle="modal" data-target="#{{$product->title}}" >Detalles</a>
+                            <button type="button" class="btn btn-success">Agregar</button>                                            
                             <!--ventana emergente-->
                             <div class="modal fade" id="{{$product->title}}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
                                 <div class="modal-dialog" role="document">
                                     <div class="modal-content">
                                         <div class="modal-header">
                                             <h4 class="modal-title" id="muModalLabel">{{$product->title}}</h4>
+                                            <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
                                         </div>
                                         <div class="modal-body">
-                                            <img src="{{ URL::to('/images/' . $product->img_route) }}" width="100" >
-                                            <h4 style="Text-align: justify;" align="justify">{{$product->slug}}</h4>  
-                                            <h4>Precio  {{number_format($product->price)}}</h4>                                  
+                                            <div align="center">
+                                            <img src="{{ URL::to('/images/' . $product->img_route) }}" width="250">
+                                            </div>
+                                            <p><h4>{{$product->slug}}</h4></p>                                 
                                         </div>
                                         <div class="modal-footer">
-                                            <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+                                            <div align="center">
+                                                <span class="label label-success"> Precio: ${{number_format($product->price)}}</span>
+                                            </div>
+                                            <button type="button" class="btn btn-success">Agregar</button>                                            
                                         </div>
                                     </div>
                                 </div>
