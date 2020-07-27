4db2c21b7081bc4ae894b3fe459da9bf0203b15e
Susana Santamaria <susana.s7@hotmail.com>
2020-07-27 13:33:15 -0500

    fix: connection database

--- config/database.php
+++ config/database.php
@@ -96,7 +96,7 @@ return [
             'host'      => env('DB_TEST_HOST', 'localhost'),
             'database'  => env('DB_TEST_DATABASE', 'homestead_test'),
             'username'  => env('DB_TEST_USERNAME', 'homestead'),
-            'password'  => env('DB_TEST_PASSWORD', 'secret'),
+            'password'  => env('DB_TEST_PASSWORD', ''),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
