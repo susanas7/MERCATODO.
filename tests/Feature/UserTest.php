e6fadbdb3a0c567dd4158e945c83b4c9d2138e6f
Susana Santamaria <susana.s7@hotmail.com>
2020-07-27 15:59:12 -0500

    test: update a user can be creaated

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -6,22 +6,30 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\User;
+use Illuminate\Support\Facades\DB;
+//use Illuminate\Foundation\Testing\WithoutMiddleware;
+
 
 class UserTest extends TestCase
 {
-    use RefreshDatabase;
-    
+    //use RefreshDatabase;
+
     /** @test */
     public function aUserCanBeCreated()
     {
-        
-        $response = $this->json('POST', '/users', [
+
+        $this->withoutExceptionHandling();
+        $this->expectException(\Illuminate\Auth\AuthenticationException::class);
+
+
+
+        /*$response = $this->json('POST', '/users', [
             'name' => 'Pupy',
             'email' => 'pupy@mail.com',
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi'
         ]);
 
-        $response->assertJsonStructure();
+        $response->assertJsonStructure();*/
 
         /* pasa con advertencia
         $user = factory(User::class)->create([
@@ -31,6 +39,18 @@ class UserTest extends TestCase
         ]);
 
         $this->actingAs($user);*/
+
+        $this->get('/users/create', [
+            'name' =>'Elisa',
+            'email' => 'elisa@mail.com',
+            'password' => '12345678'
+        ]);
+
+        $this->assertCredentials([
+            'name' =>'Elisa',
+            'email' => 'elisa@mail.com',
+            'password' => '12345678'
+        ]);
         
     }
 }
