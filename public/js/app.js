e3297aa8abb33986abb2440787a0e79079ba5d8c
Susana Santamaria <susana.s7@hotmail.com>
2020-07-27 08:49:11 -0500

    feat:add tools for sass

--- public/js/app.js
+++ public/js/app.js
@@ -49681,6 +49681,9 @@ module.exports = function(module) {
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
+var _require = __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js"),
+    nodeName = _require.nodeName;
+
 __webpack_require__(/*! ./bootstrap */ "./resources/js/bootstrap.js");
 
 window.Vue = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.common.js");
