b93e8f6a8c137b02b6d2324fd989e646dd9e371c
Susana Santamaria <susana.s7@hotmail.com>
2020-07-28 16:52:53 -0500

    test: delete products

--- tests/Feature/Products/DeleteTest.php
+++ tests/Feature/Products/DeleteTest.php
@@ -6,30 +6,31 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Product;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 
-class CreateTest extends TestCase
+class DeleteTest extends TestCase
 {
     use RefreshDatabase;
     use WithoutMiddleware;
 
     /** @test */
-    public function aProductCanBeCreated()
+    public function aProductCanBeDeleted()
     {
         $this->withoutExceptionHandling();
-
         $product = factory(Product::class)->create();
-        
 
-        $this->assertDatabaseHas('products', [
-            'title' => $product->title,
+        $this->delete('products/{$product->id}');
+
+    
+       $this->assertDatabaseMissing('products', [
+            'id' => $product->id,
             'category_id' => $product->category_id,
+            'title' => $product->title,
             'slug' => $product->slug,
+            'is_active' => $product->is_active,
             'price' => $product->price,
             'updated_at' => $product->updated_at,
             'created_at' => $product->created_at,
-        ]);
-
+       ]);
     }
 }
