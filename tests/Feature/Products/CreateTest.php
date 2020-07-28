9741a8207e4d959d64a2ecc2e45ebf66b53280b5
Susana Santamaria <susana.s7@hotmail.com>
2020-07-28 16:34:40 -0500

    test: index products

--- tests/Feature/Products/CreateTest.php
+++ tests/Feature/Products/CreateTest.php
@@ -30,5 +30,7 @@ class CreateTest extends TestCase
             'updated_at' => $product->updated_at,
             'created_at' => $product->created_at,
         ]);
+
+        $this->assertViewIs('products.create');
     }
 }
