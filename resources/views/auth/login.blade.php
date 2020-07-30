c72db7f7cb405f54d116e684a1c965d980d09624
Susana Santamaria <susana.s7@hotmail.com>
2020-07-30 14:20:05 -0500

    style: sass for register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,22 +7,33 @@
     <form method="POST" action="{{route('login')}}">
         @csrf 
         <div class="txt_field">
-            <input type="text" name="email">
+            <input type="text" name="email" class="form-control @error('email') is-invalid @enderror">
+            @error('email')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
             <span></span>
             <label>Email</label>
         </div>
         <div class="txt_field">
-            <input type="password" name="password">
+            <input type="password" name="password" class="form-control @error('password') is-invalid @enderror">
+            @error('password')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                        </span>
+            @enderror
             <span></span>
             <label>Password</label>
         </div>
         <div>
         @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-            <input type="submit" value="Login">
+            <a class="btn btn-link" href="{{ route('password.request') }}">
+                {{ __('Forgot Your Password?') }}
+            </a>
+        @endif
+        <br>
+            <input type="submit" value="Ingresar">
         </div>
     </form>
 </div>
