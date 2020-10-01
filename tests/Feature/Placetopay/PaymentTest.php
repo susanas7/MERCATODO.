fce0c1889d25301e78f5b05632ebc92c0c0eb05b
susanas7 <susana.s7@hotmail.com>
2020-10-01 14:58:23 -0500

    test: add mockery for payment

---
+++ tests/Feature/Placetopay/PaymentTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature\Placetopay;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\User;
+use App\Order;
+use Mockery;
+
+class PaymentTest extends TestCase
+{
+    /** @test */
+    public function payment()
+    {
+        $user = factory(User::class)->make();
+        $order = factory(Order::class)->make();
+
+        $payment = Mockery::mock('Dnetix\Redirection\PlacetoPay');
+        $payment->shouldReceive('request')
+            ->andReturn('isSuccessful');
+        
+        //$this->assertEquals('isSuccessful', $payment->response->isSuccessful());
+
+    }
+}
