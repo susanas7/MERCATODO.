4f1646395180e3c5301d7536646a33fb67fa1347
susanas7 <susana.s7@hotmail.com>
2020-10-09 12:45:39 -0500

    fix: factory of product category

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -8,9 +8,7 @@ $factory->define(Product::class, function (Faker $faker) {
         'title' => $faker->sentence(4),
         'slug' => $faker->slug(4),
         'price' => rand('10', '20'),
-        'category_id' => function() {
-            return factory(ProductCategory::class)->create()->id;
-        },
+        'category_id' => rand('1', '5'),
         'img_route' => $faker->imageUrl(600, 338),
     ];
 });
