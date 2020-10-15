a7ef32e5259dffd18779d0f184da1bf6abf1d20f
susanas7 <susana.s7@hotmail.com>
2020-10-15 11:40:03 -0500

    test: anUserCanUpdateTheirProfile

--- tests/Feature/Users/MyProfileTest.php
+++ tests/Feature/Users/MyProfileTest.php
@@ -23,4 +23,33 @@ class MyProfileTest extends TestCase
             ->assertStatus(200)
             ->assertSee($user->name);
     }
+
+    /** @test */
+    public function anUserCanViewTheUpdateForm()
+    {
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user)->get(route('editMyProfile'));
+
+        $response->assertOk();
+        $response->assertViewIs('users.editMyProfile');
+    }
+
+    /** @test */
+    public function anUserCanUpdateTheirProfile()
+    {
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user)->put(route('updateMyProfile'),[
+            'name' => 'JELO',
+            'email' => 'jelo@mail.com'
+        ]);
+        
+        $user = User::first();
+
+        $this->assertEquals('JELO', $user->name);
+        $this->assertEquals('jelo@mail.com', $user->email);
+        $response->assertRedirect('/myProfile?user=' .$user->id);
+
+    }
 }
