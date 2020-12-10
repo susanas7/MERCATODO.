340ff1b1f9941fb7d22f71943eb7e1a0d226acc7
Susana <susana.s7@hotmail.com>
2020-12-09 22:30:47 -0500

    fix: travis

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,31 @@
 @section('content')
 
 <section class="container-home">
-  <div class="left-navigation-home">#1</div>
-  <div class="search-input-home">#2</div>
-  <div class="content-home">#3</div>
+  <div class="left-navigation-home">
+    <div class="categories">
+      @foreach($categories as $cat)
+        <a class="btn tbn-link" href="{{route('category', $cat->id )}}">{{$cat->title}}</a><br>
+      @endforeach
+    </div>
+  </div>
+  <div class="void-navigation-home"></div>
+  <div class="search-input-home">#3</div>
+  <div class="content-home">
+      @foreach($products as $product)
+        <div class="card">
+            <a href="{{route('home.show', $product->id) }}" id="btn-show-home" >
+              <img src="{{ $product->get_image }}">
+            </a>
+            <h4>{{$product->title}}</h4>
+            <p>{{$product->slug}}</p>
+            <p class="price">$ {{number_format($product->price)}}</p>
+            @if(auth()->user())
+              <a class="add" href="{{route('user.addToCart', ['id' => $product->id ])}}">Agregar</a> 
+            @endif
+        </div>
+        {{$products->links()}} 
+      @endforeach
+  </div>
 </section>
 
 
