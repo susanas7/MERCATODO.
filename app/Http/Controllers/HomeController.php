4069bcc757dec21c343aff6b82e981ef7eaabad5
Susana <susana.s7@hotmail.com>
2020-12-10 21:58:12 -0500

    fix: travis

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -47,9 +47,9 @@ class HomeController extends Controller
      */
     public function showCategory(int $id)
     {
-        $category = ProductCategory::all();
-        $categories = Product::where('category_id', $id)->get();
+        $categories = ProductCategory::all();
+        $products = Product::where('category_id', $id)->paginate();
         $id_ = $id;
-        return view('category', compact('categories', 'id_', 'category'));
+        return view('home', compact('products', 'id_', 'categories'));
     }
 }
--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -7,7 +7,7 @@ use Faker\Generator as Faker;
 $factory->define(Product::class, function (Faker $faker) {
     return [
         'title' => $faker->sentence(1),
-        'slug' => $faker->sentence(4),
+        'slug' => $faker->sentence(9),
         'price' => rand('10', '20'),
         'category_id' => factory(ProductCategory::class)->create(),
         'is_active' => rand('0', '1'),
