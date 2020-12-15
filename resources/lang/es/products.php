069296e7c96ced487bb98aa585ebe291b6d424ea
Susana <susana.s7@hotmail.com>
2020-12-14 22:33:01 -0500

    feat(lang): add @lang for products views

---
+++ resources/lang/es/products.php
@@ -0,0 +1,37 @@
+<?php
+
+return[
+    'titles' => [
+        'index' => 'Lista de productos',
+        'create' => 'Crear un nuevo producto',
+        'update' => 'Actualizar un producto',
+    ],
+    'fields' => [
+        'products' => 'productos',
+        'page' => 'página',
+        'product' => 'Producto',
+        'slug' => 'Descripción',
+        'price' => 'Precio',
+        'status' => 'Estado',
+        'category' => 'Categoría',
+        'created_at' => 'Creado',
+        'updated_at' => 'Actualizado',
+        'active' => 'Activo',
+        'inactive' => 'Inactivo', 
+    ],
+    'messages' => [
+        'updated' => 'Producto actualizado correctamente',
+        'created' => 'Producto creado correctamente',
+        'for-import' => 'Utiliza los encabezados como se muestra en el siguiente ejemplo.',
+    ],
+    'buttons' => [
+        'create' => 'Crear',
+        'export' => 'Exportar',
+        'import' => 'Importar',
+        'view' => 'Ver',
+        'enable' => 'Activar',
+        'disable' => 'Desactivar',
+        'edit' => 'Editar',
+        'delete' => 'Eliminar',
+    ], 
+];
--- resources/views/admin/products/show.blade.php
+++ resources/views/admin/products/show.blade.php
@@ -6,52 +6,52 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Producto: {{$product->title}}</div>
+                <div class="card-header">@lang('products.fields.product'): {{$product->title}}</div>
 
                 <div class="card-body">
                         <div class="form-group row">
-                            <label name="name" class="col-md-4 col-form-label text-md-right">Nombre:</label>
+                            <label name="name" class="col-md-4 col-form-label text-md-right">@lang('products.fields.product'):</label>
 
                             <div class="col-md-6">
                             <label name="name" class="col-md-8 col-form-label text-md-center">{{$product->title}}</label>
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label name="email" class="col-md-4 col-form-label text-md-right">Descripcion:</label>
+                            <label name="email" class="col-md-4 col-form-label text-md-right">@lang('products.fields.slug'):</label>
 
                             <div class="col-md-6">
                             <label name="name" class="col-md-8 col-form-label text-md-center">{{$product->slug}}</label>
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label name="role" class="col-md-4 col-form-label text-md-right">Categoria:</label>
+                            <label name="role" class="col-md-4 col-form-label text-md-right">@lang('products.fields.category'):</label>
 
                             <div class="col-md-6">
                                 <label name="name" class="col-md-8 col-form-label text-md-center">{{ $product->category->title ?? ''}}</label>
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label name="status" class="col-md-4 col-form-label text-md-right">Estado:</label>
+                            <label name="status" class="col-md-4 col-form-label text-md-right">@lang('products.fields.status'):</label>
 
                             <div class="col-md-6">
                                 <label name="name" class="col-md-8 col-form-label text-md-center">
                                 @if($product->is_active==1)
-                                    Activo
+                                    @lang('products.fields.active')
                                 @else
-                                    Inactivo
+                                    @lang('products.fields.inactive')
                                 @endif
                                 </label>
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label name="email" class="col-md-4 col-form-label text-md-right">Creado:</label>
+                            <label name="email" class="col-md-4 col-form-label text-md-right">@lang('products.fields.created_at'):</label>
 
                             <div class="col-md-6">
                             <label name="name" class="col-md-8 col-form-label text-md-center">{{$product->created_at}}</label>
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label name="email" class="col-md-4 col-form-label text-md-right">Actualizado:</label>
+                            <label name="email" class="col-md-4 col-form-label text-md-right">@lang('products.fields.updated_at'):</label>
 
                             <div class="col-md-6">
                             <label name="name" class="col-md-8 col-form-label text-md-center">{{$product->updated_at}}</label>
